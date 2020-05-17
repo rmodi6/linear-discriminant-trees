e94c427f03f09de6d3487e0314400a911c8de2ce
Ruchit Modi <modi.ruchit6@gmail.com>
2020-05-17 00:36:55 -0400

    Bug fixes

--- scratchPad.py
+++ scratchPad.py
@@ -6,6 +6,8 @@
 
 import numpy as np
 import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 
@@ -20,23 +22,13 @@ def euclidean_distance(X1, X2):
 
     return np.sqrt(np.sum(np.square(X1 - X2)))
 
-def TreeNode():
 
-    def __init__(self):
-        self.val = None
-        self.left = None
-        self.right = None
-        
-    def __init__(self, lda, left, right):
-        self.val = lda
+class TreeNode:
+    def __init__(self, val=None, left=None, right=None):
+        self.val = val
         self.left = left
         self.right = right
-        
-    def __init__(self, label):
-        self.val = label
-        self.left = None
-        self.right = None
-        
+
     def is_leaf(self):
         return self.left is None and self.right is None
 
@@ -46,86 +38,92 @@ class LabelDetails:
         self.X = X
         self.y = y
         self.label = label
-        self.compute_means()
         self.sample_count = X.shape[0]
-        
-    def compute_means(self):
-        self.mean = self.X.describe().loc['mean'].values.reshape(-1,1)
-        
+        self.mean = self.X.describe().loc['mean'].values.reshape(-1, 1)
+
     def get_values(self):
         return self.X.values
-    
+
     def __eq__(self, other):
         """Overrides the default implementation"""
         if isinstance(other, LabelDetails):
             return self.label == other.label and (self.mean == other.mean).all()
         return False
-    
+
     def __ne__(self, other):
         ret = not self.__eq__(other)
         return ret
-        
+
     def __str__(self):
         str_ = "Class details: "
-        str_ += " - means:"+str(self.mean) + "\n"
-        str_ += " - class_name:"+self.label + "\n"
-        str_ += " - num_instances:"+str(self.samples)
+        str_ += " - means:" + str(self.mean) + "\n"
+        str_ += " - class_name:" + self.label + "\n"
+        str_ += " - num_instances:" + str(self.samples)
         return str_
-    
+
 
 class Model:
     def __init__(self):
         # Initialize class parameters
-        self.root = TreeNode()
-        self.distance_fn = Utils().euclidean_distance
-    
+        self.root = None
+
     def fit(self, X, y):
         # Function to train a decision tree for the given training data X
         label_details = self.create_labels(X, y)
         self.root = self.build_tree(label_details)
         print(str(label_details))
-        
-    def predict(self, test_sample):
-        # Function to classify test data
-        pass
-    
+
+    def predict(self, X_test):
+        y_preds = []
+        for i, row in X_test.iterrows():
+            y_preds.append(self.classify(row.values.reshape(-1, 4), self.root))
+        return y_preds
+
+    def classify(self, X, node):
+        if node.is_leaf():
+            return node.val
+        if node.val.predict(X) == 0:
+            return self.classify(X, node.left)
+        if node.val.predict(X) == 1:
+            return self.classify(X, node.right)
+
     def build_linear_discriminant(self, left_split, right_split):
-        features = None
+        features, labels = None, None
 
         for label_detail in left_split:
+            feature_values = label_detail.get_values()
             if features is None:
-                features = label_detail.get_values()
-                labels = np.zeros(features.shape[0]) # 0.0.0.0
+                features = feature_values
+                labels = np.zeros(features.shape[0])
             else:
-                features = np.append(features, label_detail.get_values())
-                labels = np.append(labels, np.zeros(features.shape[0]))
+                features = np.append(features, feature_values, axis=0)
+                labels = np.append(labels, np.zeros(feature_values.shape[0]))
 
         for label_detail in right_split:
+            feature_values = label_detail.get_values()
             if features is None:
-                features = label_detail.get_values()
-                labels = np.ones(features.shape[0]) # 1.1.1.1.
+                features = feature_values
+                labels = np.ones(features.shape[0])
             else:
-                features = np.append(features, label_detail.get_values())
-                labels = np.append(labels, np.ones(features.shape[0]))
+                features = np.append(features, feature_values, axis=0)
+                labels = np.append(labels, np.ones(feature_values.shape[0]))
 
         return LDA().fit(features, labels)
-        
-    
+
     def build_tree(self, label_details):
         if len(label_details) == 1:
             # create leaf node
             leaf_node = TreeNode(label_details[0].label)
             return leaf_node
-        
+
         left_split, right_split = self.heuristic_split(label_details)
         left_split, right_split = self.exchange(left_split, right_split, label_details)
-        
+
         left_node, right_node = self.build_tree(left_split), self.build_tree(right_split)
         lda_model = self.build_linear_discriminant(left_split, right_split)
-        
+
         return TreeNode(lda_model, left_node, right_node)
 
-    
     def heuristic_split(self, label_details):
         maximum_distance = float('-inf')
         splits = {}
@@ -144,17 +142,16 @@ class Model:
                     splits['left'].append(label_detail)
                 else:
                     splits['right'].append(label_detail)
-                    
+
         return splits['left'], splits['right']
-    
-        
+
     def exchange(self, left_split, right_split, label_details):
         maximum_information_gain = self.compute_information_gain(left_split, right_split)
         best_partition = (left_split, right_split)
         for label_detail in label_details:
             left_split_copy = left_split.copy()
             right_split_copy = right_split.copy()
-            
+
             # equals()
             if label_detail in left_split:
                 left_split_copy = [o for o in left_split_copy if o != label_detail]
@@ -162,64 +159,61 @@ class Model:
             else:
                 right_split_copy = [o for o in right_split_copy if o != label_detail]
                 left_split_copy.append(label_detail)
-        
+
             if len(left_split_copy) == 0 or len(right_split_copy) == 0:
                 continue
-        
+
             information_gain = self.compute_information_gain(left_split_copy, right_split_copy)
-        
+
             if information_gain > maximum_information_gain:
                 maximum_information_gain = information_gain
                 best_partition = (left_split_copy, right_split_copy)
 
         # TODO: remove this
-        assert(len(best_partition[0]) + len(best_partition[1]) == len(left_split) + len(right_split))
-        return best_partition 
-        
-    
+        assert (len(best_partition[0]) + len(best_partition[1]) == len(left_split) + len(right_split))
+        return best_partition
+
     def compute_information_gain(self, left_split, right_split):
         if len(left_split) == 0 or len(right_split) == 0:
             return float('-inf')
-        
+
         lda_model = self.build_linear_discriminant(left_split, right_split)
-        
+
         total_left_samples = sum([ld.sample_count for ld in left_split])
         total_right_samples = sum([rd.sample_count for rd in right_split])
         total = total_left_samples + total_right_samples
-#         print(str(total) + " " + str(total_right_samples) + " " + str(total_left_samples))
+        #         print(str(total) + " " + str(total_right_samples) + " " + str(total_left_samples))
 
         e0 = 0.0
         information_gain = 0.0
         for detail in left_split + right_split:
             e0 = e0 + self.compute_entropy(detail.sample_count, total)
-        
+
         left_predictions = []
         right_predictions = []
-        
+
         for split in [left_split, right_split]:
-            l, r = self.get_lda_predictions(split, lda)
+            l, r = self.get_lda_predictions(split, lda_model)
             left_predictions = left_predictions + l
             right_predictions = right_predictions + r
-            
-        
+
         information_gain = e0
-        for predictions, total_count in [(left_predictions, total_left_samples), (right_predictions, total_right_samples)]:
+        for predictions, total_count in [(left_predictions, total_left_samples),
+                                         (right_predictions, total_right_samples)]:
             val = 0.0
             for prediction in predictions:
                 if prediction != 0.0:
-                    temp = float(prediction)/total_count
+                    temp = float(prediction) / total_count
                     val = val + temp * np.log2(temp)
-            information_gain += val * total_count/total
-        
+            information_gain += val * total_count / total
+
         return information_gain
-    
-        
+
     def compute_entropy(self, prediction, total):
-        val = float(prediction)/total
+        val = float(prediction) / total
         return -1.0 * (val) * np.log2(val)
-            
-        
-    def get_lda_predictions(split, lda):
+
+    def get_lda_predictions(self, split, lda):
         left_predictions = []
         right_predictions = []
         for label_detail in split:
@@ -228,40 +222,41 @@ class Model:
             rows = label_detail.X.shape[0]
             X = label_detail.X.values
             for i in range(rows):
-                if lda.predict([ X[i] ]) == 0:
+                if lda.predict([X[i]]) == 0:
                     left_count += 1
                 else:
                     right_count += 1
-            
+
             left_predictions.append(left_count)
             right_predictions.append(right_count)
-            
+
         return left_predictions, right_predictions
-        
-    
+
     def create_labels(self, X, y):
         unique_classes = set(y.unique())
         label_details = []
         for class_type in unique_classes:
-            yi = y[y==class_type]
+            yi = y[y == class_type]
             indexes = yi.index
-            xi = X.iloc[indexes]
+            xi = X.loc[indexes]
             label_detail = LabelDetails(xi, yi, class_type)
-            
+
             label_details.append(label_detail)
-        
+
         return label_details
 
 
-class Driver:
-    def main(self):
-        pass
+# constants
+iris_column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+utils = Utils()
+dataset = utils.load_dataset('data/iris.data', cols=iris_column_names)
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
 
-
-
-
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 model = Model()
-ret = model.fit(X, y)
-model.p
+model.fit(X_train, y_train)
+y_preds = model.predict(X_test)
+print(accuracy_score(y_test, y_preds))
+print("Done")
